c0a957ac18bf8d678db83901c3849a775a0d9cb7
Imessal <38953768+Imessal@users.noreply.github.com>
2018-10-22 20:05:46 +0300

    даблятьсделалнахуй
    конечный автомат теперь работает, вроде даже без ошибок

--- src/main/java/FiniteStateMachine.java
+++ src/main/java/FiniteStateMachine.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Method;
 import java.util.*;
 
 
@@ -7,20 +6,28 @@ public class FiniteStateMachine{
     private static Kinoman context = null;
 
     private enum State {
-        Ready("покажи"),
-        Requesting("похожие", "следующий", "покажи"),
-        ShowingSimilar("следующий", "покажи"),
-        ShowingNext("следующий", "покажи", "похожие");
+        Ready("покажи", "помощь", "возможные запросы"),
+        Requesting("похожие", "следующий", "покажи","помощь", "возможные запросы"),
+        ShowingSimilar("следующий", "покажи","помощь", "возможные запросы"),
+        ShowingNext("следующий", "покажи", "похожие","помощь", "возможные запросы"),
+        Error("следующий", "покажи","помощь", "возможные запросы"),
+        LaunchError("покажи","помощь", "возможные запросы"),
+        ShowingHelp("покажи", "помощь", "возможные запросы"),
+        ShowingValidRequests("покажи", "помощь", "возможене запросы");
 
         State(String... in) {
             inputs = Arrays.asList(in);
         }
 
         State nextState(String input, State current) {
-            if (inputs.contains(input)) {
-                return map.getOrDefault(input, current);
+            if (inputs.contains(findRequest(input))) {
+                return map.getOrDefault(findRequest(input), current);
             }
-            return current;
+            System.out.println("Ошибка ввода: невернвый аргумент");
+            if (current.equals(State.Ready) || current.equals(State.Error)|| current.equals(State.LaunchError))
+                return State.LaunchError;
+            else
+            return State.Error;
         }
 
         final List<String> inputs;
@@ -30,6 +37,8 @@ public class FiniteStateMachine{
             map.put("покажи", State.Requesting);
             map.put("похожие", State.ShowingSimilar);
             map.put("следующий", State.ShowingNext);
+            map.put("помощь", State.ShowingHelp);
+            map.put("возможные запросы", State.ShowingValidRequests);
         }
     }
 
@@ -46,23 +55,35 @@ public class FiniteStateMachine{
                     System.out.println("Готов сделать запрос");
                     break;
                 case Requesting:
-//                    System.out.println("В данный момент реквестирую фильм");
-                    Kinoman km = Kinoman.createKinomanOnRequest(input);
-                    context = km;
+                    System.out.println("В данный момент реквестирую фильм");
+                    context = Kinoman.createKinomanOnRequest(input);
                     context.showNext();
                     break;
                 case ShowingSimilar:
-//                    System.out.println("Показываю похожие...");
+                    System.out.println("Показываю похожие...");
                     context.showSimilar();
                     break;
                 case ShowingNext:
-//                    System.out.println("Показываю следующие");
+                    System.out.println("Показываю следующие");
                     context.showNext();
                     break;
+                case ShowingHelp:
+                    Kinoman.printHelp();
+                    break;
+                case ShowingValidRequests:
+                    Kinoman.printValidRequest();
+                    break;
                 default:
-//                    System.out.println("Ошибка ввода");
+                   System.out.println("Ошибка ввода");
 
             }
         }
+    }
+    private static String findRequest(String input){
+        var request = "покажи";
+        if(input.contains(request)){
+            return request;
+        }
+        else return input;
     }
 }
