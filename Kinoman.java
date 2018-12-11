abe69579eba99bced56aa25d1dce83e1996fae57
Imessal <38953768+Imessal@users.noreply.github.com>
2018-12-11 11:36:57 +0500

    SQLException fix

---
+++ Kinoman.java
@@ -0,0 +1,182 @@
+import java.util.*;
+
+
+class Kinoman{
+    private User user;
+    private String link;
+    private int pageCount;
+    private int movieCount;
+    private int currentPage = 1;
+    private int currentMovieNumber = 0;
+    private String movieTitle = null;
+    private String sortingType = null;
+    private Movie currentMovie = null;
+    private ArrayList<Integer> shownMovieList = new ArrayList<>();
+    private Map<Integer, ArrayList<Movie>> pageList = new HashMap<>();
+    private DatabaseRepository repository;
+
+    Kinoman(User user, String request, DatabaseRepository repository){
+        this.user = user;
+        this.link = getLink(request);
+        this.movieCount = KinopoiskParser.getMoviesCount(link);
+        this.pageCount = KinopoiskParser.getPageCount(movieCount);
+        this.repository = repository;
+    }
+
+    User getCurrentUser(){return user;}
+
+    DatabaseRepository getRepository(){return repository;}
+
+    Map<Integer, ArrayList<Movie>> getPageList(){return pageList;}
+
+    Movie getNext(){
+        if (movieTitle != null) {
+            return getMovieCalled();
+        }else if (sortingType.equals("рандомный")){
+            return getRandomly();
+        } else {
+            return getInOrder();
+        }
+    }
+
+    private Movie getMovieCalled() {
+        Bot.log.config("Начал парсить по названию");
+        ArrayList<Movie> movies = getMoviesListOnPage(currentPage);
+        Bot.log.config("Закончил");
+        if (currentMovieNumber == movies.size()) {
+            return null;
+        }
+        Movie movie = movies.get(currentMovieNumber);
+        currentMovie = movie;
+        currentMovieNumber ++;
+        return movie;
+    }
+
+    private Movie getInOrder(){
+        while (true) {
+            if (currentPage > pageCount) {
+                return null;
+            }
+            ArrayList<Movie> movies = getMoviesListOnPage(currentPage);
+            Movie movie = movies.get(currentMovieNumber);
+            currentMovie = movie;
+            currentMovieNumber ++;
+            if (currentMovieNumber == movies.size()) {
+                currentPage++;
+                link = LinkBuilder.getNextPage(link);
+                currentMovieNumber = 0;
+            }
+            if (repository.checkMovie(user.getId(), movie.getId())) {
+                return movie;
+            }
+        }
+    }
+
+    private Movie getRandomly(){
+        Random rn = new Random();
+        while (true){
+            if (shownMovieList.size() == movieCount){
+                return null;
+            }
+            currentPage = rn.nextInt(pageCount) + 1;
+            link = LinkBuilder.getPageIndexOf(link, currentPage);
+            Bot.log.config("Начал парсить рандомно");
+            ArrayList<Movie> movies = getMoviesListOnPage(currentPage);
+            Bot.log.config("закончил");
+            currentMovieNumber = rn.nextInt(movies.size());
+            Movie movie = movies.get(currentMovieNumber);
+            if (!shownMovieList.contains(movie.getId())) {
+                shownMovieList.add(movie.getId());
+                currentMovie = movie;
+                if (repository.checkMovie(user.getId(), movie.getId())) {
+                    return movie;
+                }
+            }
+        }
+    }
+
+    Movie getCurrentMovie(){
+        return currentMovie;
+    }
+
+    List<Movie> showSimilar(){
+        ArrayList<Movie> similarMovies = KinopoiskParser.getSimilarMoviesList(currentMovie.getLink());
+        if (similarMovies != null && similarMovies.size() > 2) {
+            return similarMovies.subList(0, 3);
+        } else {
+            return null;
+        }
+    }
+
+    private ArrayList<Movie> getMoviesListOnPage(int page){
+        if (pageList.containsKey(page)){
+            return pageList.get(page);
+        } else if (movieTitle != null) {
+            Bot.log.config("Начал парсить");
+            pageList.put(page, KinopoiskParser.getMoviesListCalled(link));
+            Bot.log.config("Закончил");
+            return pageList.get(page);
+        } else {
+            Bot.log.config("Начал парсить");
+            pageList.put(page, KinopoiskParser.getMoviesList(link));
+            Bot.log.config("Закончил");
+            return pageList.get(page);
+        }
+    }
+
+    private String getLink(String request){
+        StringBuilder movieTitle = new StringBuilder();
+        String typeOfMovie = "фильм";
+        sortingType = "рандомный";
+        ArrayList<String> genres = new ArrayList<>();
+        ArrayList<String> countries = new ArrayList<>();
+
+        for (String word : splitRequest(request)) {
+            if (word.equals("покажи")){
+                continue;
+            }
+            if (LinkBuilder.getTypeOfMovieDict().containsKey(word)) {
+                typeOfMovie = word;
+                continue;
+            }
+            if (LinkBuilder.getSortingTypeDict().containsKey(word)) {
+                this.sortingType = word;
+                continue;
+            }
+            if (LinkBuilder.getGenreDict().containsKey(word)) {
+                genres.add(word);
+                continue;
+            }
+            if (LinkBuilder.getCountriesDict().containsKey(word)){
+                countries.add(word);
+                continue;
+            }
+            movieTitle.append(word).append(" ");
+        }
+        if (movieTitle.toString().length() == 0){
+            return LinkBuilder.getLink(typeOfMovie, sortingType,
+                    1, genres.toArray(new String[0]), countries.toArray(new String[0]));
+        }else {
+            this.movieTitle = movieTitle.toString().trim();
+            return LinkBuilder.getLink(movieTitle.toString());
+        }
+    }
+
+    static ArrayList<String> splitRequest(String request){
+        char[] chars = request.toCharArray();
+        StringBuilder word = new StringBuilder();
+        ArrayList<String> words = new ArrayList<>();
+        for (int i = 0; i < chars.length; i++){
+            if(i+1 == chars.length){
+                word.append(chars[i]);
+                words.add(word.toString());
+            }else if (chars[i] == 32 && !word.toString().equals("по")){
+                words.add(word.toString());
+                word.delete(0, word.length());
+            }else {
+                word.append(chars[i]);
+            }
+        }
+        return words;
+    }
+}
---
+++ Selector.java
@@ -0,0 +1,140 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+class Selector {
+    static Answer getAnswer(User user, String request){
+        FiniteStateMachine.State state = user.FSM.workWithRequest(request);
+        switch (state) {
+            case SayHello:
+                Bot.log.config("FSM - SayHello");
+                return sayHello();
+
+            case Requesting:
+                Bot.log.config("FSM - Requesting");
+                return requesting(user, request);
+
+            case ShowingAnnotation:
+                Bot.log.config("FSM - ShowingAnnotation");
+                return showingAnnotation(user);
+
+            case ShowingSimilar:
+                Bot.log.config("FSM - ShowingSimilar");
+                return showingSimilar(user);
+
+            case ShowingNext:
+                Bot.log.config("FSM - ShowingNext");
+                return showingNext(user);
+
+            case BanningCurrentRequest:
+                Bot.log.config("FSM - BanningRequest");
+                return banningCurrentRequest(user);
+
+            case ShowingHelp:
+                Bot.log.config("FSM - ShowingHelp");
+                return showingHelp();
+
+            case ShowingValidRequests:
+                Bot.log.config("FSM - ShowingValidRequests");
+                return showingValidRequests();
+
+            default:
+                return new Answer(new String[]{"Некорректная команда" +
+                        "\nМожет ты что-то не правильно написал?"},
+                        new String[]{"Помощь", "Возможные запросы"});
+        }
+    }
+
+    private static Answer sayHello(){
+        return new Answer(new String[]{"Привет, я Киноман!\nМогу помочь с выбором фильма, сериала и т.д.\n" +
+                "Для справки введи \"помощь\""},
+                new String[]{"Помощь", "Возможные запросы"});
+    }
+    private static Answer requesting(User user, String request){
+        user.kinoman = new Kinoman(user, request, new DatabaseRepository());
+        Movie movie = user.kinoman.getNext();
+        if (movie == null){
+            return new Answer(new String[]{"Ты пытался найди хоррор мультик или что-то вроде?" +
+                    "\nВ любом случае я ничего не нашел."});
+        }
+        return new Answer(new String[]{movie.toStringMovie()},
+                new String[]{"Похожие", "Следующий"}, new String[]{"Скрыть", "Аннотация"});
+    }
+    private static Answer showingAnnotation(User user){
+        return new Answer(new String[]{user.kinoman.getCurrentMovie().getAnnotation()},
+                new String[]{"Скрыть", "Следующий"},  new String[]{"Похожие"});
+    }
+    private static Answer showingSimilar(User user){
+        List<Movie> similarMovie = user.kinoman.showSimilar();
+        if(similarMovie != null) {
+            ArrayList<String> answer = new ArrayList<String>() {
+            };
+            for (Movie curMovie: similarMovie){
+                answer.add(curMovie.toStringMovie());
+            }
+            return new Answer(answer.toArray(new String[0]),
+                    new String[]{"Следующий"});
+        } else {
+            return new Answer(new String[]{"Я не нашел похожих, сорян"},
+                    new String[]{"Следующий"});
+        }
+    }
+    private static Answer showingNext(User user){
+        Movie movie = user.kinoman.getNext();
+        if (movie == null){
+            return new Answer(new String[]{"Это был последний"});
+        }
+        return new Answer(new String[]{movie.toStringMovie()},
+                new String[]{"Похожие", "Следующий"}, new String[]{"Скрыть", "Аннотация"});
+    }
+    private static Answer banningCurrentRequest(User user){
+        Movie c_movie = user.kinoman.getCurrentMovie();
+        DatabaseRepository repository = user.kinoman.getRepository();
+        repository.addMovieToBlackList(user, c_movie);
+        return new Answer(new String[]{"Больше я не буду показывать вам - " + c_movie.getName()},
+                new String[]{"Похожие", "Следующий"});
+    }
+    private static Answer showingHelp(){
+        return new Answer(new String[]{"Просто напиши - \"Покажи\" + что хочешь посмотреть\n" +
+                "\nПример запроса:\n" +
+                "Покажи Назад в будущее\n" +
+                "Покажи новые мультфильмы\n" +
+                "Покажи американские сериалы по рейтингу\n\n" +
+                "Команды:\n" +
+                "Помощь - выводит справку\n" +
+                "Возможные запросы - выводит возможные фильтры и сортировки\n" +
+                "Покажи ... - выводит фильмы в соответствии с запросом\n" +
+                "Следующий - выводит следующий фильм в соответствии с запросом\n" +
+                "Аннотация - выводит аннотацию к фильму\n" +
+                "Похожие - выводит 3 фильма, похожих на последний\n" +
+                "Скрыть - добавляет фильм в черный список\n"},
+                new String[]{"Возможные запросы"});
+    }
+    private static Answer showingValidRequests(){
+        StringBuilder st = new StringBuilder();
+        st.append("*Могу найти фильм по названию*\n");
+        st.append("*Могу найти*: ");
+        for (String movieType : LinkBuilder.getTypeOfMovieDict().keySet()){
+            st.append(movieType).append(", ");
+        }
+        st.deleteCharAt(st.length() - 2);
+
+        st.append("\n*Выводить могу*: ");
+        for (String sortingType : LinkBuilder.getSortingTypeDict().keySet()){
+            st.append(sortingType).append(", ");
+        }
+        st.deleteCharAt(st.length() - 2);
+
+        st.append("\n*Знаю такие жанры, как*: ");
+        for (String genre : LinkBuilder.getGenreDict().keySet()){
+            st.append(genre).append(", ");
+        }
+        st.deleteCharAt(st.length() - 2);
+
+        st.append("\n*Знаю такие страны, как*: ");
+        for (String country : LinkBuilder.getCountriesDict().keySet()){
+            st.append(country).append(", ");
+        }
+        return new Answer(new String[]{st.deleteCharAt(st.length() - 2).toString()});
+    }
+}
